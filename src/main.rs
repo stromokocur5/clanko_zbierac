4c245e013a4acac4978edd9d8ff6130b5c3087b4
stromokocur5 <gabrielkohut3@proton.me>
2024-01-04 21:56:25 +0100

    neviem co mam napisat ale je to cistaaaa fantaaaazia

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ async fn main() -> Result<()> {
     let config = config_from_file()?;
     let client = trend::get_client(config).await?;
     let test = trend::get_article(&client,reqwest::Url::parse("https://trend.sk/biznis/tesla-nestiha-drzat-krok-tronu-mladom-trhu-coskoro-ujme-novy-lider-ciny?itm_brand=trend&itm_template=listing&itm_modul=articles-rubric-list&itm_position=6")?).await?;
+    let test = discord::html_to_markdown(&test)?;
     println!("{test}");
 
     Ok(())
